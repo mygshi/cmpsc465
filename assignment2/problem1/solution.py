89943e58df4820bc9e11041a217d8945045811ab
djs6744 <70667607+djs6744@users.noreply.github.com>
2020-09-20 00:47:52 -0400

    Update solution.py

--- assignment2/problem1/solution.py
+++ assignment2/problem1/solution.py
@@ -1,19 +1,18 @@
 length = int(input())
 array = []
 
-for i in range(0, length):
-    array.append(input().split())
-
+array.append(input().split())
+    
 i = 0
 j = 0
 counter = 0
 
 while i < length:
     if i < j:
-        if int(array[i][0]) > int(array[j][0]):
+        if int(array[0][i]) > int(array[0][j]):
            counter += 1
     j += 1
-    if j == int(len(array)):
+    if j == int(length):
         i += 1
         j = i
 
