65081c15d3b16f40adafaa9dc2b5992aac8a2ce9
djs6744 <70667607+djs6744@users.noreply.github.com>
2020-09-19 20:30:14 -0700

    Update solution.py

--- assignment2/problem1/solution.py
+++ assignment2/problem1/solution.py
@@ -1,1 +1,20 @@
-asdf
+length = int(input())
+array = []
+
+for i in range(0, length):
+    array.append(input())
+
+i = 0
+j = 0
+counter = 0
+
+while i < length:
+    if i < j:
+        if int(array[i]) > int(array[j]):
+           counter += 1
+    j += 1
+    if j == int(len(array)):
+        i += 1
+        j = i
+
+print(counter)
