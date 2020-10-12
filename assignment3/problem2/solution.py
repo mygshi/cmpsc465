33165717754b0902a47426475ade6bef247f6f40
djs6744 <70667607+djs6744@users.noreply.github.com>
2020-10-12 17:13:28 -0400

    Update solution.py

--- assignment3/problem2/solution.py
+++ assignment3/problem2/solution.py
@@ -6,18 +6,41 @@ from queue import PriorityQueue
 import heapq
 import math
 
-class PriorityQueue(s):
-    
-    S[0] = s
+"""
+This is Pseudocode from class
 
-    def empty():
+Algorithm Dijkstra (G = (V;E), l(e) for any e 2 E, s 2 V)
+    dist[v] = infinity, for any v 2 V;
+    prev[v] = null, for any v in V;
+    init an empty priority queue PQ;
+    for any v in V: insert (PQ, v), where the priority of v is infinity;
+    dist[s] = 0;
+    decrease-key (PQ; s; 0);
+    while (empty (PQ) = false)
+        u = find-min (PQ);
+        delete-min (PQ);
+        for each edge (u;v) in E
+            if (dist[v] > dist[u]+l(u,v))
+                dist[v] = dist[u]+l(u,v);
+                prev[v] = u;
+                decrease-key (PQ, v, dist[v]);
+            end if;
+        end for;
+    end while;
+end algorithm;
+"""
+
+"""
+class PriorityQueue:
+    
+    def empty(PQ):
         if (n == 0): return True
         else: return False
     
     # Incomplete
-    def insert(x):
+    def insert(PQ, x):
         n = n + 1
-        S[n] = x
+        S.append(x)
         bubble_up(S, n)
     
     # Incomplete
@@ -25,13 +48,13 @@ class PriorityQueue(s):
         return S[0]
 
     # Incomplete
-    def delete_min():
+    def delete_min(PQ):
         S[0] = S[n - 1]
         n = n - 1
         sift_down(S, 1)
 
     # Incomplete
-    def decrease_key(k, new_key):
+    def decrease_key(PQ, k, new_key):
         S[k].key = new_key
         bubble_up(S, k)
     
@@ -44,39 +67,48 @@ class PriorityQueue(s):
     
     # Incomplete
     def sift_down(S, k):
-        
+
 
     def swap(p, k):
         temp = S[p - 1]
         S[p - 1] = S[k - 1]
         S[k - 1] = temp
+    """
 
 
 
-def dijkstra(n, m, s):
+def dijkstra(n, m, s, l, v):
     infinity = 99999
 
     dist = [infinity] * n
     prev = [None] * n
-    PQ = PriorityQueue(s)
-
+    #PQ = PriorityQueue()
+    heap = []
+    heapq.heapify(heap)
 
     # for any v in V: insert (PQ, v), where the priority of v is infinity
     for i in range(0, n):
-        PQ.insert(n + 1)
+        heapq.heappush(heap, i + 1)
+
 
     dist[s - 1] = 0
-    print(dist)
-    PQ.decrease_key(s, 0)
+    #PQ.decrease_key(PQ, s, 0)
 
-    while(PQ.empty() == False):
-        u = PQ.find_min()
-        PQ.delete_min()
+    while(heap): #PQ.empty() == False
+        #u = PQ.find_min()
+        u = heap[0]
+        #PQ.delete_min(PQ)
+        heapq.heappop(heap)
         for i in range(0, m):
-            if (dist[v - 1] > dist[u - 1] + c):
-                dist[v - 1] = dist[u - 1] + c
-                prev[v - 1] = u
-                PQ.decrease_key(v, dist[v - 1])
+            
+            if (dist[v[i] - 1] > dist[u - 1] + l[i]):
+                dist[v[i] - 1] = dist[u - 1] + l[i]
+                prev[v[i] - 1] = u
+                #PQ.decrease_key(PQ, v, dist[v - 1])
+
+    for i in range(0, n):
+        print(dist[v[i] - 1])
+                
 
 # Mainframe
 line1 = input().split()
@@ -84,8 +116,17 @@ n = int(line1[0])
 m = int(line1[1])
 s = int(line1[2])
 
-dijkstra(n, m, s)
+l = []
+u = []
+v = []
 
-# Incomplete
 for i in range(0, m):
-    c = 1
+    line = input().split()
+    a = int(line[0])
+    b = int(line[1])
+    c = int(line[2])
+    u.append(a)
+    v.append(b)
+    l.append(c)
+    
+dijkstra(n, m, s, l, v)
