242bb885dc45a03b463f9872984ea9807d00dfa7
Michael <mygshi@gmail.com>
2020-10-12 12:52:53 -0400

    First Save

--- assignment3/problem1/solution.py
+++ assignment3/problem1/solution.py
@@ -1,1 +1,25 @@
-#Dominick Spadafore, Vivian Chen, Michael Shi
+#Dominick Spadafore
+#Vivian Chen
+#Michael Shi mxs977
+
+from collections import defaultdict
+
+def first_dfs(graph, u, visited, stack):
+	vis(u) = 1
+	for v in graph(u):
+		if vis(v) == False:
+			dfs1(graph, v, visited, stack)
+		stack.append(u)
+
+def second_dfs(graph, u ,visited):
+	visited[u] = 1
+	for v in graph[u]:
+		if vis[v] == False:
+			dfs2(graph, v, visited)
+
+def reverse_graph(graph, n):
+	rev_graph = defaultdict(list)
+	for u in range(1, n_+ 1):
+		for v in graph[u]:
+			rev_graph[v].append(u)
+	return new_graph
