c778e60a4490fbc9c55bdaaebe88ec82e82f2ff6
djs6744 <70667607+djs6744@users.noreply.github.com>
2020-10-14 10:08:26 -0400

    Update solution.py

--- assignment3/problem2/solution.py
+++ assignment3/problem2/solution.py
@@ -17,32 +17,45 @@ def dijkstra(n, m, s, l, E, v):
 
     # for any v in V: insert (PQ, v), where the priority of v is infinity
     for i in range(0, n):
-        heapq.heappush(PQ, (infinity, i + 1))
+        if(i == s - 1):
+            heapq.heappush(PQ, (0, i + 1))
+        else:
+            heapq.heappush(PQ, (infinity, i + 1))
+
+    #print(PQ)
+    #heapq.heappush(PQ, (0, 5))
+    #print(PQ)
         
     dist[s - 1] = 0
     #PQ.decrease_key(PQ, s, 0)
     #heapq._siftdown(PQ, s, 0)
-    print(PQ)
+    #print(PQ)
     
     while(PQ): #PQ.empty() == False
         #u = PQ.find_min()
         u = PQ[0][1]
         #PQ.delete_min(PQ)
         heapq.heappop(PQ)
+        #print(PQ)
         for i in range(len(E)):
-            print("i =", i)
-            print("(u, v) =", u, v[i])
+            #print("(u, v) =", u, v[i])
             if([u, v[i]] in E):
                 index = E.index([u, v[i]])
-                print("(u, v) =", u, v[index], "length =", l[index])
-                print("index =", index) 
+                #print("(u, v) =", u, v[index], "length =", l[index])
+                #print("index =", index) 
                 
             else:
+                #index = i
                 continue
-
+            
+            
             if (dist[v[i] - 1] > dist[u - 1] + l[index]):
                 dist[v[i] - 1] = dist[u - 1] + l[index]
                 prev[v[i] - 1] = u
+                #print(PQ)
+                if(dist[v[i] - 1] > 0):
+                    heapq.heappush(PQ, (dist[v[i] - 1], v[i]))
+                
 
     for i in range(0, len(dist)):
         print(dist[i])
