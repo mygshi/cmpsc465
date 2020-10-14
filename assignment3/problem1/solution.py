c0823156bd536a9f8e24156dcaa2e945e399262b
Michael <mygshi@gmail.com>
2020-10-14 13:04:27 -0400

    Update

--- assignment3/problem1/solution.py
+++ assignment3/problem1/solution.py
@@ -1,30 +1,30 @@
-#Dominick Spadafore
-#Vivian Chen
+#Dominick Spadafore djs6744
+#Vivian Chen vyc5073
 #Michael Shi mxs977
 
 from collections import defaultdict
 
 # DFS for graph
-def first_dfs(graph, u, visited, stack):
-	visited[u] = True
-	for v in graph[u]:
-		if visited[v] == False:
-			first_dfs(graph, v, visited, stack)
-		stack.append(u)
+def first_dfs(graph, v, visited, arr):
+	visited[v] = True
+	for e in graph[v]:
+		if visited[e] == False:
+			first_dfs(graph, e, visited, arr)
+		arr.append(v)
 
 # DFS for reversed graph
-def second_dfs(graph, u ,visited):
-	visited[u] = True
-	for v in graph[u]:
-		if visited[v] == False:
-			second_dfs(graph, v, visited)
+def second_dfs(graph, v ,visited):
+	visited[v] = True
+	for e in graph[v]:
+		if visited[e] == False:
+			second_dfs(graph, e, visited)
 
 # Transposes the inputted graph and returns the reversed version
 def reverse_graph(graph, vertices):
 	new_graph = defaultdict(list)
-	for u in range(0, vertices):
-		for v in graph[u]:
-			new_graph[v].append(u)
+	for i in range(0, vertices):
+		for j in graph[i]:
+			new_graph[j].append(i)
 	return new_graph
 
 # Function to count the connected components
@@ -32,12 +32,14 @@ def count(graph, vertices):
 	arr = []
 	count = 0
 	reversed_graph = reverse_graph(graph, vertices)
+	# Initializes all of the variables to not have been visited
 	visited = [False] * (vertices + 1)
 	for i in range(0, vertices):
 		if visited[i] == False:
 			first_dfs(graph, i, visited, arr)
+	# Sets them all back to false for the second dfs through
 	visited = [False] * (vertices + 1)
-	while len(arr) > 0:
+	while len(arr) != 0:
 		last_element = arr.pop()
 		if visited[last_element] == False:
 			count += 1
