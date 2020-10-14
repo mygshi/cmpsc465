ef757aad06adc51ac89bc9fd48b83d1d88544f48
Michael <mygshi@gmail.com>
2020-10-14 11:39:43 -0400

    Count off by 1

--- assignment3/problem1/solution.py
+++ assignment3/problem1/solution.py
@@ -3,28 +3,57 @@
 #Michael Shi mxs977
 
 from collections import defaultdict
-line = input().split()
-vertices = int(line[0])
-edges = int(line[1])
 
+# DFS for graph
 def first_dfs(graph, u, visited, stack):
-	visited[u] = 1
+	visited[u] = True
 	for v in graph[u]:
 		if visited[v] == False:
-			dfs1(graph, v, visited, stack)
+			first_dfs(graph, v, visited, stack)
 		stack.append(u)
 
+# DFS for reversed graph
 def second_dfs(graph, u ,visited):
-	visited[u] = 1
+	visited[u] = True
 	for v in graph[u]:
 		if visited[v] == False:
-			dfs2(graph, v, visited)
+			second_dfs(graph, v, visited)
 
-def reverse_graph(graph, n):
-	rev_graph = defaultdict(list)
-	for u in range(1, n_+ 1):
+# Transposes the inputted graph and returns the reversed version
+def reverse_graph(graph, vertices):
+	new_graph = defaultdict(list)
+	for u in range(1, vertices + 1):
 		for v in graph[u]:
-			rev_graph[v].append(u)
+			new_graph[v].append(u)
 	return new_graph
 
-def count(graph, n);
+# Function to count the connected components
+def count(graph, vertices):
+	arr = []
+	count = 0
+	reversed_graph = reverse_graph(graph, vertices)
+	visited = [False] * (vertices + 1)
+	for i in range(1, vertices + 1):
+		if visited[i] == False:
+			first_dfs(graph, i, visited, arr)
+	visited = [False] * (vertices + 1)
+	while len(arr) > 0:
+		last_element = arr.pop()
+		if visited[last_element] == False:
+			count += 1
+			second_dfs(reversed_graph, last_element, visited)
+	return count
+
+# Main
+line1 = input().split()
+vertices = int(line1[0])
+edges = int(line1[1])
+graph = defaultdict(list)
+
+for i in range(0, edges):
+	line = input().split()
+	u = int(line[0])
+	v = int(line[1])
+	graph[u].append(v)
+
+print(count(graph, vertices))
