30858ad9517182c41b760a19a492892718f93438
djs6744 <70667607+djs6744@users.noreply.github.com>
2020-10-14 20:36:20 -0400

    Update solution-1.py

--- assignment3/problem2/solution-1.py
+++ assignment3/problem2/solution-1.py
@@ -2,58 +2,62 @@
 #Vivian Chen vyc5073
 #Michael Shi mxs977
 
-class Graph(): 
-  
-    def __init__(self, n): 
-        self.V = n 
-        self.graph = [[0 for i in range(n)]  
-                    for j in range(n)]
+import heapq
 
-    def find_min(self, dist, prev):
+def dijkstra(n, m, s, l, E, v):
+    infinity = 99999
 
-        min = 999999
-        index = 0
+    dist = [infinity] * n
+    prev = [None] * n
+    PQ = []
+    heapq.heapify(PQ)
 
-        for v in range(self.V):
-            if dist[v] < min and prev[v] == None:
-                min = dist[v]
-                index = v
+    for i in range(0, n):
+        if(i == s - 1):
+            heapq.heappush(PQ, (0, i + 1))
+        else:
+            heapq.heappush(PQ, (infinity, i + 1))
+        
+    dist[s - 1] = 0
+    
+    while(PQ):
+        u = PQ[0][1]
+        heapq.heappop(PQ)
+        for i in range(len(E)):
+            if([u, v[i]] in E):
+                index = E.index([u, v[i]])
+                
+            else:
+                continue
+            if (dist[v[i] - 1] > dist[u - 1] + l[i]):
+                dist[v[i] - 1] = dist[u - 1] + l[i]
+                prev[v[i] - 1] = u
 
-        return index
+                if(dist[v[i] - 1] > 0):
+                    heapq.heappush(PQ, (dist[v[i] - 1], v[i]))
 
-    def dijkstra(self, s): 
+    for i in range(0, len(dist)):
+        print(dist[i])
 
-        dist = [999999] * self.V
-        dist[s] = 0
-        prev = [None] * self.V 
-  
-        for cout in range(self.V): 
-  
-            u = self.find_min(dist, prev)
-  
-            prev[u] = True
-  
-            for v in range(self.V): 
-                if self.graph[u][v] > 0 and prev[v] == None and dist[v] > dist[u] + self.graph[u][v]: 
-                        dist[v] = dist[u] + self.graph[u][v]
-  
-        for i in range(self.V):
-            if(dist[i] == 999999):
-                dist[i] = -1
-            print(dist[i]) 
-  
 # Mainframe
 line1 = input().split()
 n = int(line1[0])
 m = int(line1[1])
 s = int(line1[2])
-g = Graph(n)
+
+l = []
+u = []
+v = []
+E = []
 
 for i in range(0, m):
     line = input().split()
     a = int(line[0])
     b = int(line[1])
     c = int(line[2])
-    g.graph[a-1][b-1] = c
+    u.append(a)
+    v.append(b)
+    l.append(c)
+    E.append([a,b])
 
-g.dijkstra(s-1)
+dijkstra(n, m, s, l, E, v)
