3c4b49b8ecb8621170d786edd13a26d003fc2d57
djs6744 <70667607+djs6744@users.noreply.github.com>
2020-10-14 20:34:15 -0400

    Update solution.py

--- assignment3/problem2/solution.py
+++ assignment3/problem2/solution.py
@@ -2,42 +2,37 @@
 #Vivian Chen vyc5073
 #Michael Shi mxs977
 
-import heapq
+def find_min(dist, prev):
 
-def dijkstra(n, m, s, l, E, v):
-    infinity = 99999
+        min = 999999
+        index = 0
 
-    dist = [infinity] * n
-    prev = [None] * n
-    PQ = []
-    heapq.heapify(PQ)
+        for v in range(n):
+            if dist[v] < min and prev[v] == None:
+                min = dist[v]
+                index = v
 
-    for i in range(0, n):
-        if(i == s - 1):
-            heapq.heappush(PQ, (0, i + 1))
-        else:
-            heapq.heappush(PQ, (infinity, i + 1))
-        
-    dist[s - 1] = 0
-    
-    while(PQ):
-        u = PQ[0][1]
-        heapq.heappop(PQ)
-        for i in range(len(E)):
-            if([u, v[i]] in E):
-                index = E.index([u, v[i]])
-                
-            else:
-                continue
-            
-            if (dist[v[i] - 1] > dist[u - 1] + l[index]):
-                dist[v[i] - 1] = dist[u - 1] + l[index]
-                prev[v[i] - 1] = u
-                if(dist[v[i] - 1] > 0):
-                    heapq.heappush(PQ, (dist[v[i] - 1], v[i]))
-                
+        return index
 
-    for i in range(0, len(dist)):
+def dijkstra(s, n, graph): 
+
+    dist = [999999] * n
+    dist[s] = 0
+    prev = [None] * n 
+  
+    for cout in range(n): 
+  
+        u = find_min(dist, prev)
+  
+        prev[u] = True
+  
+        for v in range(n): 
+            if graph[u][v] > 0 and prev[v] == None and dist[v] > dist[u] + graph[u][v]: 
+                dist[v] = dist[u] + graph[u][v]
+  
+    for i in range(n):
+        if(dist[i] == 999999):
+            dist[i] = -1
         print(dist[i])
 
 # Mainframe
@@ -45,20 +40,15 @@ line1 = input().split()
 n = int(line1[0])
 m = int(line1[1])
 s = int(line1[2])
-
-l = []
-u = []
-v = []
-E = []
+graph = []
+for i in range(0, n):
+    graph.append([0]*n)
 
 for i in range(0, m):
     line = input().split()
     a = int(line[0])
     b = int(line[1])
     c = int(line[2])
-    u.append(a)
-    v.append(b)
-    l.append(c)
-    E.append([a,b])
+    graph[a-1][b-1] = c
 
-dijkstra(n, m, s, l, E, v)
+dijkstra(s-1, n, graph)
